59419b808920960b3962d0b28246e60e1bb72340
DmytroKhmaruk <dima.khmaruk7@gmail.com>
2024-05-03 18:01:30 +0200

    add backend Register, Login, Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -12,7 +13,10 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err);
     });
 
-app.use("/api/user", userRoute);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server is running!")
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,27 @@
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("user test is Successfull");
-});
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
 
-router.post("userposttest", (req, res) => {
-    const username = req.body.username;
-    console.log(username);
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json("You are have trouble!");
+    }
 });
 
 module.exports = router;

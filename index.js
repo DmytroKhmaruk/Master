b58e6907966b2323a60192e7df63e1dc54f0bc99
DmytroKhmaruk <dima.khmaruk7@gmail.com>
2024-05-07 14:03:43 +0200

    +

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -13,9 +17,13 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err);
     });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
